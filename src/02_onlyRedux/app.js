0b25322e6704d43224acce97f5ece8eb405178b2
Renaldy <renalfrontend@gmail.com>
2021-02-14 23:54:25 +0700

    Only Redux, Combine Reducer

--- src/02_onlyRedux/app.js
+++ src/02_onlyRedux/app.js
@@ -1,11 +1,17 @@
 const redux = require('@reduxjs/toolkit');
-const initialState = {
-   numOfCakes : 10,
-   numOfIceCream : 20
-};
+const createStore = redux.createStore;
+const combineReducer = redux.combineReducers; // Mengkombinasi banyak fungsi reducer
 
 const BUY_CAKE = 'BUY_CAKE';
-const BUY_ICECREAM = 'BUY_ICE_CREAM'
+const BUY_ICECREAM = 'BUY_ICECREAM'
+
+const initialCakeState = {
+   numOfCakes : 10,
+};
+
+const initialIceCreamState = {
+   numOfIceCream : 20
+}
 
 const buyCake = () => {
    return {
@@ -21,31 +27,37 @@ const buyIceCream= () => {
    }
 }
 
-function cakeReducer(state=initialState, action) {
+function cakeReducer(state=initialCakeState, action) {
    switch(action.type) {
       case BUY_CAKE : return {
+         ...state, 
          numOfCakes : state.numOfCakes - 1
       }
       default : return state
    }
 }
 
-function iceCreamReducer(state=initialState, action) {
+function iceCreamReducer(state=initialIceCreamState, action) {
    switch(action.type) {
       case BUY_ICECREAM : return {
+         ...state,
          numOfIceCream : state.numOfIceCream - 1
       }
       default : return state
    }
 }
 
-const createStore = redux.createStore;
-const store = createStore(iceCreamReducer);
+let rootReducer = combineReducer({ // Membuat route kombinasi reducer
+   cake : cakeReducer,
+   iceCream : iceCreamReducer
+})
 
-console.log('initial state : ', store.getState());
+const store = createStore(rootReducer);
+
+console.log('initial state    : ', store.getState());
 
 const unsubscribe = store.subscribe(() => {   
-   console.log(store.getState()); 
+   console.log('update state : ', store.getState()); 
 });
 
 store.dispatch(buyIceCream());  // ini dicatat
